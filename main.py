35f7d812b84d8ed002e98bd6afc91b676aee2c4c
JakubSzwajka <szwajkajakub@gmail.com>
2021-03-10 00:56:13 +0100

    port define

--- main.py
+++ main.py
@@ -47,5 +47,5 @@ if __name__ == "__main__":
     for i in range(len(DATABASE_TMP)):
         ip_database[str(i)] = DATABASE_TMP[i]
     
-    app.run(debug=True,host='0.0.0.0') 
+    app.run(debug=True,host='0.0.0.0', port = int(os.environ.get('PORT',5000))) 
 
