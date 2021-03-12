5b02e231d767cadb092e426df163ac5c33c49b4c
JakubSzwajka <szwajkajakub@gmail.com>
2021-03-12 18:19:47 +0100

    prod config ready

--- app/main/config.py
+++ app/main/config.py
@@ -12,10 +12,8 @@ class DevelopmentConfig(Config):
 
 
 class ProductionConfig(Config):
-    DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = 'postgres://bvahrjumxffaet:fb698577cefb50b44f549bfb6ec3106736ae71219d2c3f69aeae7b8e361caf05@ec2-54-155-87-214.eu-west-1.compute.amazonaws.com:5432/d38t2r3sfa67np'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///../../db/database.db'
-    # SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
 
 class TestingConfig(Config):
     DEBUG = True
