d9d98d9dd9221baf52342e91091238015d2df3e3
JakubSzwajka <szwajkajakub@gmail.com>
2021-03-12 16:31:51 +0100

    manager added

---
+++ manage.py
@@ -0,0 +1,35 @@
+import os
+import unittest
+
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+from flask_restful import Api
+
+from app.main import create_app, db
+from app.main.controller.ip_address_controller import Ip_address_controller 
+
+app = create_app(os.getenv('CONFIG') or 'dev')
+app.app_context().push()
+
+api = Api(app)
+api.add_resource(Ip_address_controller, "/ip")
+
+manager = Manager(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+
+@manager.command
+def run():
+    app.run()
+
+@manager.command
+def test():
+    """Runs the unit tests."""
+    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+    result = unittest.TextTestRunner(verbosity=2).run(tests)
+    if result.wasSuccessful():
+        return 0
+    return 1
+
+if __name__ == '__main__':
+    manager.run()
