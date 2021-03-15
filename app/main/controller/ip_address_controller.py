4f47f0ba6e4af3f787adfc3a6f08d52802ac7147
JakubSzwajka <szwajkajakub@gmail.com>
2021-03-15 15:33:25 +0100

    test

--- app/main/controller/ip_address_controller.py
+++ app/main/controller/ip_address_controller.py
@@ -41,22 +41,19 @@ class Ip_address_controller(Resource):
 
     @token_required
     def put(self):
-        return {"message": "test"}, 200
         args = request.json 
         
         # multiple objs
         if "data" in args.keys():
-
             try: 
-                # new_ip_obj = create_new_ip_addresses(data=args)
+                new_ip_obj = create_new_ip_addresses(data=args)
                 # response_list = []
                 # for i, new_obj in enumerate(new_ip_obj):
                 #     dict_obj = dict(marshal(new_obj, single_ip_model))
                 #     filtered = { key: value for key, value in dict_obj.items() if value is not None}
                 #     response_list.append(filtered)
-                return 'some message'
-                # message = json.dumps({ "response": marshal(new_ip_obj, single_ip_model)}) 
-                # return Response(message, status = 201, mimetype = 'application/json')
+                return { "response" : marshal(new_ip_obj, single_ip_model)}, 200 
+                
             except Database_error as error:
                 abort(error.error_code, message = str(error))
                 
