c347e02bd49f1a2413c27ccf82706e8d4c3d9e4c
JakubSzwajka <szwajkajakub@gmail.com>
2021-03-15 14:53:52 +0100

    test

--- app/main/controller/ip_address_controller.py
+++ app/main/controller/ip_address_controller.py
@@ -47,15 +47,15 @@ class Ip_address_controller(Resource):
         if "data" in args.keys():
 
             try: 
-                new_ip_obj = create_new_ip_addresses(data=args)
+                # new_ip_obj = create_new_ip_addresses(data=args)
                 # response_list = []
                 # for i, new_obj in enumerate(new_ip_obj):
                 #     dict_obj = dict(marshal(new_obj, single_ip_model))
                 #     filtered = { key: value for key, value in dict_obj.items() if value is not None}
                 #     response_list.append(filtered)
-                    
-                message = json.dumps({ "response": marshal(new_ip_obj, single_ip_model)}) 
-                return Response(message, status = 201, mimetype = 'application/json')
+                return 'some message'
+                # message = json.dumps({ "response": marshal(new_ip_obj, single_ip_model)}) 
+                # return Response(message, status = 201, mimetype = 'application/json')
             except Database_error as error:
                 abort(error.error_code, message = str(error))
                 
